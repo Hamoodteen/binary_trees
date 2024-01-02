a82ef55dc30802ddfa7009940db776affe91c7aa
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 01:12:27 +0200

    0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,15 +7,20 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t L_height, R_height;
+	size_t L_height = 0, R_height = 0;
 
-	if (tree == NULL) {
+	if (tree == NULL)
 		return (0);
-	}
 	else
 	{
-		L_height = binary_tree_height(tree->left);
-		R_height = binary_tree_height(tree->right);
-		return (L_height > R_height) ? (L_height + 1) : (R_height + 1);
+		if (tree->left != NULL)
+		{
+			L_height = binary_tree_height(tree->left);
+		}
+		if (tree->right != NULL)
+		{
+			R_height = binary_tree_height(tree->right);
+		}
 	}
+	return (L_height > R_height) ? (L_height + 1) : (R_height + 1);
 }
