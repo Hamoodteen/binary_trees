e9cf6d0755c8052821555b6c5bc0018188485fd8
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 00:26:48 +0200

    0

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,7 +8,14 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	(void)parent;
-	(void)value;
-	return (NULL);
+	binary_tree_t *node;
+
+	node = binary_tree_node(parent, value);
+	if (parent->right != NULL)
+	{
+		parent->right->parent = node;
+		node->right = parent->right;
+	}
+	parent->right = node;
+	return (node);
 }
