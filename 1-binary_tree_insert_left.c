16133384ab6272575b9f43357a5e2a02ba550284
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 00:18:52 +0200

    0

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,9 +14,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if ((node == NULL) || (parent == NULL))
 		return (NULL);
 	node->n = value;
-	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
+	node->parent = parent->left;
 	parent->left = node;
 	return (node);
 }
