0de6d3b14deac329406e294968d697bd6dfefe92
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 01:00:48 +0200

    0

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,6 +8,13 @@
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	(void)tree;
-	(void)func;
+	if ((tree != NULL) || (func != NULL))
+	{
+	}
+	else
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
