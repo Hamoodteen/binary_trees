33c3fd136a5d578da8bd64c822d04aa85c076c83
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 01:43:10 +0200

    0

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,6 +7,15 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	(void)tree;
-	return (0);
+	size_t sizep = 0, sizel = 0, sizer = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent != NULL)
+		sizep = 1 + binary_tree_size(tree->parent);
+	if (tree->left != NULL)
+		sizel = 1 + binary_tree_size(tree->left);
+	if (tree->right != NULL)
+		sizer = 1 + binary_tree_size(tree->right);
+	return (sizep + sizel + sizer);
 }
