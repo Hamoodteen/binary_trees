da9fc80fb18e1ea17fa622aa4cbde0f04caed046
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 00:35:56 +0200

    0

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,5 +10,13 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 	{
 	}
+	if (tree->left)
+	{
+	binary_tree_delete(tree->left);
+	}
+	if (tree->right)
+	{
+	binary_tree_delete(tree->right);
+	}
 	free(tree);
 }
