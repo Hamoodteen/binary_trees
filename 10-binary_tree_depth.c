92800f4a474350ee8d29fcc0ea099cacc31edac4
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 01:32:32 +0200

    0

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,6 +12,6 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->parent != NULL)
-		depth = 1 + binary_tree_height(tree->parent);
+		depth = 1 + binary_tree_depth(tree->parent);
 	return (depth);
 }
