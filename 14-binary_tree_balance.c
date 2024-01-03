953ae14e287125bac245fad512a78b61afd10037
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 02:28:25 +0200

    0

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,6 +7,18 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	(void)tree;
-	return (0);
+	size_t L_height = 0, R_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL)
+		L_height = 1 + binary_tree_balance(tree->left);
+	if (tree->right != NULL)
+		R_height = 1 + binary_tree_balance(tree->right);
+	if (L_height > R_height)
+		return (1);
+	else if (L_height < R_height)
+		return (-1);
+	else
+		return (0);
 }
