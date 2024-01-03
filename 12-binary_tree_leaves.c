c5aea93a16633394a7c6fb11b154a08ffc854c21
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 02:19:40 +0200

    0

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	else if ((tree->left == NULL) && (tree->right == NULL))
-        return (1);
+		return (1);
 	calcl = binary_tree_leaves(tree->left);
 	calcr = binary_tree_leaves(tree->right);
 	return (calcl + calcr);
