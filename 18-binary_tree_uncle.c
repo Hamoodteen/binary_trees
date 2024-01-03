c950493468ea0a3ec51eee09b3abc40b92deafb9
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 03:57:28 +0200

    0

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,9 +9,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if ((node == NULL) || (node->parent == NULL))
 		return (NULL);
-	if (node->parent == node->parent->parent->left)
+	if ((node->parent->parent->right != NULL) &&
+	(node->parent == node->parent->parent->left))
 		return (node->parent->parent->right);
-	else if (node->parent == node->parent->parent->right)
+	else if ((node->parent->parent->left != NULL) &&
+	(node->parent == node->parent->parent->right))
 		return (node->parent->parent->left);
 	else
 		return (NULL);
