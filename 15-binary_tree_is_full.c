f4638c22243d6514244050ae2f812458e50fc9c2
Hamoodteen <ahmed104567@outlook.com>
2024-01-03 02:58:57 +0200

    0

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,7 +7,9 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if ((tree == NULL) || ((tree->left == NULL) && (tree->right == NULL)))
+	if (tree == NULL)
+		return (1);
+	else if ((tree->left == NULL) && (tree->right == NULL))
 		return (1);
 	else if ((tree->left != NULL) && (tree->right != NULL))
 		return ((binary_tree_is_full(tree->left)) &&
